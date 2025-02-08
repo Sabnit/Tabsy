661d99cb399ee1519fdf51ec0f217339b30f4303
Sabnit <sabnit111@gmail.com>
2025-02-08 23:26:51 +0545

    feat: add category storage and retrieval Store categories in localStorage Retreive and display categories in the category list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-// main.js
+import { createCategoryListEl } from "./utils.js";
+
+// Input fields
+const inputCategoryEl = document.getElementById("category");
+
+// Buttons
+const saveCategoryBtn = document.getElementById("save-category");
+
+// ul elements
+const categoryUlEl = document.getElementById("ul-category");
+
+// Initialize the lists
+let categoryList = [];
+
+function main() {
+  categoryList = JSON.parse(localStorage.getItem("categoryList"));
+
+  createCategoryListEl(categoryList, categoryUlEl);
+}
+
+// Category Section
+saveCategoryBtn.addEventListener("click", () => {
+  let categoryName = inputCategoryEl.value;
+
+  // Validation
+  if (!categoryName) {
+    console.log("Category is empty");
+    return;
+  }
+
+  categoryList.push(categoryName);
+
+  createCategoryListEl(categoryList, categoryUlEl);
+
+  localStorage.setItem("categoryList", JSON.stringify(categoryList));
+});
+
+main();
--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,15 @@
-// utils.js
+// Create category lists
+export function createCategoryListEl(items, ulEl) {
+  ulEl.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+
+    a.href = "#";
+    a.textContent = items[i];
+    a.classList.add("tabList");
+
+    li.appendChild(a);
+    ulEl.appendChild(li);
+  }
+}
