e4c5b21d3551c2f88602bd580b2d8772468d0a55
Sabnit <sabnit111@gmail.com>
2025-02-09 16:21:57 +0545

    feat: toogle category items and navigate back to category list

--- js/main.js
+++ js/main.js
@@ -1,21 +1,51 @@
-import { createCategoryListEl } from "./utils.js";
+import { createCategoryListEl, createTabsList } from "./utils.js";
 
 // Input fields
 const inputCategoryEl = document.getElementById("category");
 
 // Buttons
-const saveCategoryBtn = document.getElementById("save-category");
+const saveCategoryBtn = document.getElementById("add-category");
 
 // ul elements
 const categoryUlEl = document.getElementById("ul-category");
+const categoryItemsUlEl = document.getElementById("ul-category-items");
+
+// div element
+const categoryInputContainer = document.getElementById("category-input-field");
+const categoryItemsContainer = document.getElementById(
+  "category-items-container"
+);
+
+const backEl = document.getElementById("back-el");
 
 // Initialize the lists
 let categoryList = [];
 
 function main() {
-  categoryList = JSON.parse(localStorage.getItem("categoryList"));
+  categoryList = JSON.parse(localStorage.getItem("categoryList")) || [];
 
   createCategoryListEl(categoryList, categoryUlEl);
+
+  let categoryItems = document.querySelectorAll("#ul-category li");
+
+  categoryItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      categoryItems.forEach((el) => {
+        el.style.display = "none";
+      });
+      item.style.display = "block";
+      getCategoryItems(item.textContent);
+      backEl.style.display = "block";
+    });
+  });
+}
+
+function getCategoryItems(categoryName) {
+  let items = Object.entries(JSON.parse(localStorage.getItem(categoryName)));
+
+  createTabsList(items, categoryItemsUlEl);
+  categoryItemsContainer.style.display = "block";
+  categoryInputContainer.style.display = "none";
 }
 
 // Category Section
@@ -33,6 +63,18 @@ saveCategoryBtn.addEventListener("click", () => {
   createCategoryListEl(categoryList, categoryUlEl);
 
   localStorage.setItem("categoryList", JSON.stringify(categoryList));
+  localStorage.setItem(categoryName, "{}");
+
+  inputCategoryEl.value = "";
+
+  main();
+});
+
+backEl.addEventListener("click", () => {
+  main();
+  categoryItemsContainer.style.display = "none";
+  backEl.style.display = "none";
+  categoryInputContainer.style.display = "block";
 });
 
 main();
--- js/utils.js
+++ js/utils.js
@@ -3,13 +3,31 @@ export function createCategoryListEl(items, ulEl) {
   ulEl.innerHTML = "";
   for (let i = 0; i < items.length; i++) {
     let li = document.createElement("li");
+    li.textContent = items[i];
+    li.classList.add("categoryList");
+    ulEl.appendChild(li);
+  }
+}
+
+// Create tabs lists
+export function createTabsList(items, ulEl) {
+  ulEl.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    console.log(items[i][0]);
+    console.log(items[i][1]);
+
+    let li = document.createElement("li");
     let a = document.createElement("a");
 
-    a.href = "#";
-    a.textContent = items[i];
+    a.href = items[i][1];
+    a.textContent = items[i][0];
+    a.target = "_blank";
+
     a.classList.add("tabList");
 
     li.appendChild(a);
     ulEl.appendChild(li);
   }
+
+  console.log(ulEl);
 }
